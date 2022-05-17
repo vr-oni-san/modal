7511d2a8bdefceb2362f442421ad8f7c9ae79007
machikawa <a1938hm@aiit.ac.jp>
2022-05-17 21:39:02 +0900

    テキストの横いっぱいとる方法をようやくマスタあ

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,21 +4,29 @@ import 'modal.dart';
 Column QuizBody(BuildContext context) {
   return Column(
     children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("下のアイコンをクリックすると・・・", style: TextStyle(
-              fontSize: 15
-          ),),
-        ],
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+             width: double.infinity,
+             height: 200,
+             color: Colors.deepOrange,
+             child: Text("下のアイコンをクリックすると・・・何が起きるのでしょうか",
+               style: TextStyle(
+                 fontSize: 30
+             ),),
+           ),
       ),
+      Divider(),
+      // 解答欄
+
+      // 解説 or 次/前
       Divider(),
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           InkWell(
             child:
-              Text("aaa",
+              Text("設問",
                 style: TextStyle(
                   fontSize: 30
                 ),
